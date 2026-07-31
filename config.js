8ea20438a952d309ea0005525c74f8e3a9e454b2
contatofalapopular-bit <contatofalapopular@gmail.com>
2026-07-31 14:19:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,20 @@
 const CONFIG = Object.freeze({
   APP_NAME: "Central Rádios Brasil",
-  VERSION: "3.1.4",
+  VERSION: "3.2.0",
   GITHUB_OWNER: "contatofalapopular-bit",
   DADOS_REPO: "CentralRadiosBrasil-Dados",
   DADOS_BRANCH: "main",
+  WORKER_URL:
+    "https://broken-bar-45e2.contatofalapopular.workers.dev",
+  ADMIN_KEY_SESSION_STORAGE:
+    "crb-admin-chave-sessao-v1",
   REQUEST_TIMEOUT_MS: 15000,
   LOCAL_STORAGE_KEY: "crb-admin-radios-rascunho-v1",
   ESTADOS_STORAGE_KEY: "crb-admin-estados-rascunho-v1",
   CATEGORIAS_STORAGE_KEY: "crb-admin-categorias-rascunho-v1",
   CIDADES_STORAGE_KEY: "crb-admin-cidades-cache-v1",
-  IBGE_MUNICIPIOS_URL: "https://servicodados.ibge.gov.br/api/v1/localidades/municipios",
+  IBGE_MUNICIPIOS_URL:
+    "https://servicodados.ibge.gov.br/api/v1/localidades/municipios",
   STREAMS_STORAGE_KEY: "crb-admin-streams-rascunho-v1",
   EMISSORAS_STORAGE_KEY: "crb-admin-emissoras-rascunho-v1",
   RADIOS_SCHEMA_VERSION: "3.1.4",
